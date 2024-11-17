f75064129a677965dbaf3661b224b58d553355ac
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-11-17 15:24:09 +0100

    add_sharp

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
-const { Jimp } = require("jimp");
-const { resolve } = require("path");
+const sharp = require("sharp");
+const path = require("path");
 
 // sprawdzamy czy folder istnieje, jesli nie to tworzymy go:
 const isAccessible = (path) =>
@@ -26,36 +26,45 @@ const setUpFolder = async (path) => {
 const MAX_AVATAR_WIDTH = 512;
 const MAX_AVATAR_HEIGHT = 512;
 
-const isImageAndTransform = async (path) =>
-  new Promise((resolve) => {
-    Jimp.read(path, async (err, image) => {
-      if (err) resolve(false);
+const isImageAndTransform = async (filePath) => {
+  try {
+    const image = sharp(filePath);
 
-      try {
-        const w = image.getWidth();
-        const h = image.getHeight();
-        const cropWidth = w > MAX_AVATAR_WIDTH ? MAX_AVATAR_WIDTH : w;
-        const cropHeight = h > MAX_AVATAR_HEIGHT ? MAX_AVATAR_HEIGHT : h;
-        const centerX = Math.round(w / 2 - cropWidth / 2);
-        const centerY = Math.round(h / 2 - cropHeight / 2);
+    const metadata = await image.metadata();
 
-        await image
-          // rotate to walidacja, jeśli ktoś przyslalby nam inny plik niz zdjęcie, to nie udałyby się poniższe działania na tym pliku
-          .rotate(360)
-          .crop(
-            centerX < 0 ? 0 : centerX,
-            centerY < 0 ? 0 : centerY,
-            cropWidth,
-            cropHeight
-          )
-          .sepia()
-          .write(path);
-        resolve(true);
-      } catch (e) {
-        console.log(e);
-        resolve(false);
-      }
-    });
-  });
+    const w = metadata.width;
+    const h = metadata.height;
+
+    const cropWidth = w > MAX_AVATAR_WIDTH ? MAX_AVATAR_WIDTH : w;
+    const cropHeight = h > MAX_AVATAR_HEIGHT ? MAX_AVATAR_HEIGHT : h;
+
+    const centerX = Math.round(w / 2 - cropWidth / 2);
+    const centerY = Math.round(h / 2 - cropHeight / 2);
+
+    const tempOutputPath = path.join(
+      path.dirname(filePath),
+      `temp-${path.basename(filePath)}`
+    );
+
+    await image
+      .resize(cropWidth, cropHeight)
+      .extract({
+        left: centerX,
+        top: centerY,
+        width: cropWidth,
+        height: cropHeight,
+      })
+      .rotate(360)
+      .toFile(tempOutputPath);
+
+    await fs.rename(tempOutputPath, filePath);
+
+    console.log("Image processed successfully");
+    return true;
+  } catch (err) {
+    console.log("Error during image processing:", err);
+    return false;
+  }
+};
 
 module.exports = { setUpFolder, isImageAndTransform };
